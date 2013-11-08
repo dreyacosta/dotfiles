403103bd01eaa4bc2af513b34f9cdcd9a3a02b28
David Rey <david.rey.acosta@gmail.com>
2013-11-09 00:22:28 +0100

    New install deps script

--- install-deps.sh
+++ install-deps.sh
@@ -1,37 +1,42 @@
-# up to you (me) if you want to run this as a file or copy paste at your leisure
+curl -L https://get.rvm.io | bash -s stable
 
-
-# https://github.com/jamiew/git-friendly
-# the `push` command which copies the github compare URL to my clipboard is heaven
-sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
-
-# https://rvm.io
-# rvm for the rubiess
-curl -L https://get.rvm.io | bash -s stable --ruby
-
-# https://github.com/isaacs/nave
-# needs npm, obviously.
-# TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
-npm install -g nave
-
-
-# homebrew!
-# you need the code CLI tools YOU FOOL.
-ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
-
-
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 
 # https://github.com/rupa/z
 # z, oh how i love you
 mkdir -p ~/code/z
 curl https://raw.github.com/rupa/z/master/z.sh > ~/code/z/z.sh
 chmod +x ~/code/z/z.sh
-# also consider moving over your current .z file if possible. it's painful to rebuild :)
 
+brew doctor
 
-# now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
-# then you grab public URL and send off your video message in a heartbeat.
+brew install wget
+brew install htop
+brew install nmap
+brew install node
+brew install mongodb
 
+brew tap phinze/homebrew-cask
+brew install brew-cask
 
-# for the c alias (syntax highlighted cat)
-sudo easy_install Pygments
+brew cask install virtualbox
+brew cask install sublime-text
+brew cask install google-chrome
+brew cask install google-drive
+brew cask install opera
+brew cask install firefox
+brew cask install firefox-nightly
+brew cask install opera-next
+brew cask install team-viewer
+
+wget https://github.com/dreyacosta/dotfiles/archive/master.zip
+
+unzip master.zip
+
+mv -v dotfiles-master/.* .
+
+rm -rfv dotfiles-master/
+
+rm -rf  ~/Library/Application\ Support/Sublime\ Text\ 2/
+
+ln -s ~/Google\ Drive/Sublime ~/Library/Application\ Support/Sublime\ Text\ 2
