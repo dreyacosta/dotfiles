f8cd5adf604d0bc125803364855528d7c151c830
David Rey <david.rey.acosta@gmail.com>
2014-07-23 02:20:40 +0200

    Updated install script

--- install-deps.sh
+++ install-deps.sh
@@ -1,5 +1,3 @@
-curl -L https://get.rvm.io | bash -s stable
-
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 # https://github.com/rupa/z
@@ -15,7 +13,7 @@ brew install ngrep
 brew install htop
 brew install nmap
 brew install git bash-completion
-brew install node
+brew install nvm
 brew install mongodb
 
 sudo npm install -g grunt-cli
@@ -26,13 +24,11 @@ brew tap dreyacosta/homebrew-cask
 brew install brew-cask
 
 brew cask install dropbox
-brew cask install magicprefs
 brew cask install virtualbox
-brew cask install vagrant
 brew cask install sublime-text
 brew cask install iterm2
 brew cask install google-chrome
-brew cask install google-chrome-canary
+# brew cask install google-chrome-canary
 brew cask install google-hangouts
 brew cask install opera
 brew cask install opera-next
