440f077a344dfa2652017ad26c32629f6a2e40e2
David Rey <david.rey.acosta@gmail.com>
2025-08-18 23:11:51 +0200

    add neovim cspell lsp

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -12,6 +12,23 @@ return {
             workingDirectory = { mode = "auto" },
           },
         },
+        vim.lsp.enable("cspell_ls"),
+        vim.lsp.config("cspell_ls", {
+          cmd = { "cspell-lsp", "--stdio" },
+          filetypes = {
+            "lua",
+            "python",
+            "javascript",
+            "typescript",
+            "html",
+            "css",
+            "json",
+            "yaml",
+            "markdown",
+            "gitcommit",
+          },
+          root_markers = { ".git" },
+        }),
       },
       setup = {
         eslint = function()
