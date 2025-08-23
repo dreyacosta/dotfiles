b035a2af7ddf878a9a3c2d4616fec1431b8fede0
David Rey <david.rey.acosta@gmail.com>
2025-08-23 19:25:43 +0200

    update obsidian setup

--- config/nvim/lua/plugins/obsidian.lua
+++ config/nvim/lua/plugins/obsidian.lua
@@ -20,8 +20,12 @@ return {
   opts = {
     workspaces = {
       {
-        name = "personal",
-        path = "~/Dropbox/Obsidian",
+        name = "box",
+        path = "~/Projects/obsidian/box",
+      },
+      {
+        name = "notes",
+        path = "~/Projects/obsidian/notes",
       },
     },
 
@@ -32,24 +36,14 @@ return {
       min_chars = 2,
     },
 
+    preferred_link_style = "markdown",
+    disable_frontmatter = true,
+
     -- Optional, customize how note IDs are generated given an optional title.
     ---@param title string|?
     ---@return string
     note_id_func = function(title)
-      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-      -- In this case a note with the title 'My new note' will be given an ID that looks
-      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-      local suffix = ""
-      if title ~= nil then
-        -- If title is given, transform it into valid file name.
-        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-      else
-        -- If title is nil, just add 4 random uppercase letters to the suffix.
-        for _ = 1, 4 do
-          suffix = suffix .. string.char(math.random(65, 90))
-        end
-      end
-      return tostring(os.time()) .. "-" .. suffix
+      return tostring(title)
     end,
   },
 }
