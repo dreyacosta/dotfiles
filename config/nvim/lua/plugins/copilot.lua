2fd23748f6132c5f8aa69a5b44571eaf0f419b85
David Rey <david.rey.acosta@gmail.com>
2026-02-17 18:22:44 +0100

    simplify nvim copilot config

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,10 +1,22 @@
 return {
   {
-    "github/copilot.vim",
-    enabled = vim.g.ai_cmp,
-  },
-  {
     "zbirenbaum/copilot.lua",
     enabled = vim.g.ai_cmp,
+    opts = {
+      logger = {
+        file = vim.fn.stdpath("log") .. "/copilot-lua.log",
+        file_log_level = vim.log.levels.DEBUG,
+        print_log_level = vim.log.levels.WARN,
+        trace_lsp = "verbose",
+        trace_lsp_progress = true,
+        log_lsp_messages = true,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = false,
+        keymap = { accept = "<Tab>" },
+      },
+    },
   },
 }
